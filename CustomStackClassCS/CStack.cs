d965acb69a87a5d46d868bd77e134076d525b618
Angel Barranco <angelusualle@gmail.com>
2018-05-02 09:21:01 -0400

    Adding dat

---
+++ CustomStackClassCS/CStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStackClassCS
+{
+    class CStack<T>
+    {
+        public class CStackNode<T>
+        {
+            public T data { get; set; }
+            public CStackNode<T> next { get; set; }
+            public CStackNode(T data)
+            {
+                this.data = data;
+            }
+        }
+        private CStackNode<T> top;
+        public T pop()
+        {
+            T datat = top.data;
+            top = top.next;
+            return datat;
+        }
+        public void push(T item)
+        {
+            CStackNode<T> topt = top;
+            top = new CStackNode<T>(item);
+            top.next = topt;
+        }
+        public T peek()
+        {
+            return top.data;
+        }
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+    }
+}
---
+++ CustomStackClassCS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStackClassCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CStack<string> stack = new CStack<string>();
+            stack.push("Hello");
+            stack.push("There");
+            stack.push("Bro");
+            while (stack.isEmpty() != true)
+            {
+                Console.WriteLine(stack.pop());
+            }
+
+        }
+    }
+}
